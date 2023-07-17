6c0f85da8c4447a70ba5b9c4a28dd6b10a2cc722
William Redenbaugh <wmredenbaugh@gmail.com>
2023-07-16 23:34:59 -0700

    implemented time

--- chal_includes.h
+++ chal_includes.h
@@ -2,5 +2,6 @@
 #define _CHAL_INCLUDES_H
 
 #include "chal_struct_definitions.h"
+#include "chal_time.h"
 
 #endif
---
+++ chal_time.h
@@ -0,0 +1,8 @@
+#ifndef _CHAL_TIME_H
+#define _CHAL_TIME_H
+
+#include "Arduino.h"
+
+#define get_current_time_millis() millis()
+
+#endif
