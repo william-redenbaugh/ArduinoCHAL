0d184f47c5fdfa6772e1070ac0f6c8bbb8de0b93
William Redenbaugh <wmredenbaugh@gmail.com>
2023-10-02 21:19:38 -0700

    fixing logging

--- os_misc.h
+++ os_misc.h
@@ -3,11 +3,11 @@
 #include "Arduino.h"
 // Macro: println(e)
 // Description: Prints the given expression followed by a newline character.
-#define os_println(e...) Serial.println(e)
+#define os_println(e, ...) Serial.println(e)
 
 // Macro: print(e)
 // Description: Prints the given expression.
-#define print(e...) Serial.printf(e)
+#define print(e, ...) Serial.printf(e)
 
 // Macro: os_printf(e)
 // Description: Prints the formatted output using the Serial.printf function.
